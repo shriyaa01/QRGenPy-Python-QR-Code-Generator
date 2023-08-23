e0d1629f4f7501186bb944e51acaf8184851997f
Shriya Srivastava <100456530+shriyaa01@users.noreply.github.com>
2023-08-23 16:59:57 +0530

    qrGenerator2.py

--- qrGenerator2.py
+++ qrGenerator2.py
@@ -1,1 +1,9 @@
+# Import the qrcode library as 'qr'
+import qrcode as qr
+
+# Generate a QR code with the provided URL
+img = qr.make("https://github.com/shriyaa01/QRGenPy-Python-QR-Code-Generator")
+
+# Save the generated image to a file
+img.save("Code.png")
 

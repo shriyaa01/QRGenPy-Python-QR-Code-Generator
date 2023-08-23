35173ddd7e96761d9963c70b5bf3d286a500dd74
Shriya Srivastava <100456530+shriyaa01@users.noreply.github.com>
2023-08-23 16:55:59 +0530

    initial commit

--- qrGenerator1.py
+++ qrGenerator1.py
@@ -1,1 +1,19 @@
+# Import the necessary libraries
+import qrcode
+from PIL import Image
+
+# Create a QR code object with specified settings
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=4)
+
+# Add the data (URL) to the QR code
+qr.add_data("https://www.linkedin.com/in/shriya-srivastava-90555421")
+
+# Generate the QR code pattern
+qr.make(fit=True)
+
+# Create an image from the QR code object with customized colors
+img = qr.make_image(fill_color="#FFA500", back_color="#006400")
+
+# Save the generated image to a file
+img.save("ShriyaSri_linkdln.png")
 
